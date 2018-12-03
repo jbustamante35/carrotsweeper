9e34528da66b89b0f5a9f74a9309af4518dff49c
Julian Bustamante <jbustamante35@gmail.com>
2018-12-03 01:38:59 -0600

    Saving and Visualizing pipeline output, added getDirName helper, sample output data
    Former-commit-id: 0e1303b90a0f7baba26cd505fad19c4155ea28dc
    Former-commit-id: 89976410d99e5fa9d146d5801dca28c4356ddcea

--- midlines/carrotExtraction/myversion/carrotExtractor.m
+++ midlines/carrotExtraction/myversion/carrotExtractor.m
@@ -1,45 +1,111 @@
-function [mline, cntr, sMsk, pMsk] = carrotExtractor(vis)
-%% Load file list of binary mask images
-fldr  = '/home/jbustamante/Dropbox/EdgarSpalding/projects/carrotsweeper';
-din   = 'data/development_181011/binary_mask';
-dOut    = 'output';
+function [mline, cntr, smsk, pmsk] = carrotExtractor(vis, svData, svFigs)
+%% carrotExtractor: midline extraction and straightener
+%
+%
+% Input:
+%   vis: boolean visualize outputs
+%   svData: boolean to save output in .mat file
+%   svFigs: boolean to save figures of straightened carrots
+%
+% Output:
+%   mline: cell array of midline data
+%   cntr; cell array of contour data
+%   smsk; cell array of straightened mask images
+%   pmsk: cell array of processed mask images
+%
 
+%% Load file list of binary mask images
+fldr   = '/home/jbustamante/Dropbox/EdgarSpalding/projects/carrotsweeper';
+din    = 'data/development_181011/binary_mask';
 dataIn = sprintf('%s/%s', fldr, din);
-dataOut = sprintf('%s/%s', dataIn, dOut);
+
+if svData || svFigs
+    dOut    = sprintf('output_%s', tdate('s'));
+    dataOut = sprintf('%s/%s', dataIn, dOut);
+    mkdir(dataOut);
+end
+
 ext  = '.png';
 fin  = imageDatastore(dataIn, 'FileExtensions', ext);
 
 %% Extract Midline, Contour, Straightened Image, Straightened Mask
-[mline, cntr, pMsk, sMsk] = deal(cell(1, numel(fin.Files)));
-for e = 1 : numel(fin.Files)
+tot                       = numel(fin.Files);
+[mline, cntr, pmsk, smsk] = deal(cell(1, tot));
+for n = 1 : tot
     try
-        pMsk{e}             = extendDimension(fin.readimage(e), 0);
-        pMsk{e}             = double(imcomplement(pMsk{e}));
-        [mline{e}, cntr{e}] = getMidlineAndContour(pMsk{e}, vis);
-        sMsk{e}             = sampleStraighten(mline{e}, flip(pMsk{e}, 2), vis);
+        % Prepare mask for extraction functions
+        pmsk{n}             = extendDimension(fin.readimage(n), 0);
+        pmsk{n}             = double(imcomplement(pmsk{n}));
         
-        if vis && e < numel(fin.Files)
+        % Run processed mask through extraction functions
+        [mline{n}, cntr{n}] = getMidlineAndContour(pmsk{n}, vis);
+        smsk{n}             = sampleStraighten(mline{n}, flip(pmsk{n}, 2), vis);
+        
+        % Clear figure axis
+        if vis && n < tot
             cla;clf;
         end
         
-    catch ME
-        fprintf(2, 'Error\n%s\n', ME.getReport);
+    catch e
+        fprintf(2, 'Error in Carrot Extraction Pipeline\n%s\n', e.getReport);
     end
 end
 
+%% Show Output of processed and straightened masks
+if vis
+    psec  = 0.7;
+    fig   = figure;
+    set(fig, 'Color', 'w');
+    
+    for n = 1 : tot
+        subplot(211);
+        imshow(pmsk{n});
+        ttlP = sprintf('Processed Mask\nCarrot %d', n);
+        title(ttlP);
+        
+        subplot(212);
+        flp = handleFLIP(smsk{n}, 2);
+        imshow(flp);
+        ttlS = sprintf('Straighted Mask\nCarrot %d', n);
+        title(ttlS);
+        
+        pause(psec);
+        
+        % Save figures in output directory
+        if svFigs
+            fName = getDirName(din);
+            fnm   = sprintf('%s/straightCarrot%d_%s', dataOut, n, fName);
+            savefig(fig, fnm);
+            saveas(fig, fnm, 'tiffn');
+        end
+    end
+end
+
+%% Save Data in output directory
+if svData
+    fName   = getDirName(din);
+    CARROTS = v2struct(mline, cntr, smsk, pmsk);
+    nm      = sprintf('%s/%s_carrotExtractor_%s_%dCarrots', ...
+        dataOut, tdate('s'), fName, tot);
+    save(nm, '-v7.3', 'CARROTS');
+end
 
 end
 
 function pad = extendDimension(msk, val)
-%% Extend dimension of mask by defined number of pixels
-THRESH = 100;
-chk = size(msk) - THRESH;
-idx = chk < 0;
+%% Extend dimension of mask by defined number of pixels in val
+% The threshold parameter THERSH defines theminimum length of either dimension. 
+% If a dimension is lower than the threshold value, it is padded to match the 
+% threshold value with the value defined in the val parameter. This function 
+% automatically detects which dimension should be padded. 
+THRESH = 200;
+chk    = size(msk) - THRESH;
+idx    = chk < 0;
 
 if idx
     pad = msk;
 else
-    pad = padarray(msk, chk(idx), val);
+    pad = padarray(msk, abs(chk(idx)), val);
 end
 
 end

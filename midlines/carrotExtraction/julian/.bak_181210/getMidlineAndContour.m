ecbc9a10759a1c8f6c76af78a0842f86584033c9
Julian Bustamante <jbustamante35@gmail.com>
2018-12-10 16:55:18 -0600

    Updated julian directory to include updated phytomorph code
    Former-commit-id: c1459031ddb2f93393b82c92a24821a64e285225 [formerly 144a2eaf3d5bc6a9d38c9f059c34e76f02854de7]
    Former-commit-id: 8a3e5f0c6e555b85a8335ca6e012adf14eb707df
    Former-commit-id: cf02fe3269cc052dc10827552468538475bffdc1

---
+++ midlines/carrotExtraction/julian/.bak_181210/getMidlineAndContour.m
@@ -0,0 +1,23 @@
+function [mline, cntr] = getMidlineAndContour(msk, vis)
+
+try
+    out = isolate_carrot_Roots(msk, vis, [], []);
+    %     out = isolateRoots(msk, vis, [], []);
+    mline = out(1).midlines.data';
+    
+    rm = mline(:,1) < 300;
+    mline(rm,:) = [];
+    mline(:,1) = mline(:,1) - 300;
+    
+    cntr = out(1).contours.data';
+    rm = cntr(:,1) < 300;
+    cntr(rm,:) = [];
+    cntr(:,1) = cntr(:,1) - 300;
+    
+catch e
+    fprintf(2, 'Error extracting Midline and Contour\n%s\n', e.getReport);
+    mline = [];
+    cntr = [];
+end
+
+end
